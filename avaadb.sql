5eacd26a481b68078a3a467c0dc3b990385f74bd
BugVogel <bugvogel@gmail.com>
2017-06-20 10:51:18 -0300

    BD atualizadov2

--- avaadb.sql
+++ avaadb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 07-Maio-2017 às 20:28
+-- Generation Time: 20-Jun-2017 às 15:47
 -- Versão do servidor: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
@@ -27,25 +27,24 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `aluno` (
-  `Nome` varchar(50) NOT NULL,
-  `Curso` varchar(50) NOT NULL,
+  `Nome` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `Curso` varchar(50) CHARACTER SET utf8 NOT NULL,
   `Semestre` int(2) NOT NULL,
-  `Email` varchar(50) NOT NULL,
-  `Senha` varchar(20) NOT NULL,
-  `HashCode` int(50) NOT NULL,
-  `Email Confirmado` tinyint(4) NOT NULL
+  `Email` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `Senha` varchar(20) CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `aluno`
 --
 
-INSERT INTO `aluno` (`Nome`, `Curso`, `Semestre`, `Email`, `Senha`, `HashCode`, `Email Confirmado`) VALUES
-('Rafael da Silva Macêdo', 'Engenharia', 9, '3011rafael@gmail.com', '123456', 0, 1),
-('Gabriel', 'Engenharia', 10, 'gsm_fsa2008@hotmail.com', '123', 0, 1),
-('teste', 'tt', 1, 't@gmail.com', '11', 0, 1),
-('f', 'ds', 0, 'teste@gmail.com', '123', 0, 0),
-('teste', 'teste', 1, 'teste@teste.com', '123', 0, 1);
+INSERT INTO `aluno` (`Nome`, `Curso`, `Semestre`, `Email`, `Senha`) VALUES
+('Rafael da Silva Mac?do', 'Engenharia', 9, '3011rafael@gmail.com', '123456'),
+('Bruno Gonzaga de Mattos Vogel', 'Engenharia de ComputaÃ§Ã£o', 3, 'bugvogel@gmail.com', '123456'),
+('Gabriel', 'Engenharia', 10, 'gsm_fsa2008@hotmail.com', '123'),
+('teste', 'tt', 1, 't@gmail.com', '11'),
+('f', 'ds', 0, 'teste@gmail.com', '123'),
+('teste', 'teste', 1, 'teste@teste.com', '123');
 
 -- --------------------------------------------------------
 
@@ -55,7 +54,7 @@ INSERT INTO `aluno` (`Nome`, `Curso`, `Semestre`, `Email`, `Senha`, `HashCode`, 
 
 CREATE TABLE `atividade` (
   `ID` int(3) NOT NULL,
-  `Descricao` varchar(100) NOT NULL,
+  `Descricao` varchar(100) CHARACTER SET utf8 NOT NULL,
   `Nivel` int(2) NOT NULL,
   `N_Blocos` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -65,11 +64,9 @@ CREATE TABLE `atividade` (
 --
 
 INSERT INTO `atividade` (`ID`, `Descricao`, `Nivel`, `N_Blocos`) VALUES
-(11, 'Primeiro desafio', 1, 3),
-(12, 'Segundo desafio', 1, 3),
-(13, 'Segundo desafio', 1, 3),
-(14, 'Terceiro desafio', 1, 3),
-(15, 'Testando', 1, 3);
+(15, 'Testando', 1, 3),
+(16, 'O algoritmo abaixo serÃ¡ para testar o funcionamento do banco de dados em relaÃ§Ã£o a esta funcional', 1, 3),
+(17, 'testttttttttteeeeeeeeeeee', 1, 3);
 
 -- --------------------------------------------------------
 
@@ -88,7 +85,9 @@ CREATE TABLE `atividade_turma` (
 
 INSERT INTO `atividade_turma` (`ID_atividade`, `ID_turma`) VALUES
 (15, 8),
-(15, 10);
+(16, 10),
+(17, 7),
+(17, 10);
 
 -- --------------------------------------------------------
 
@@ -98,12 +97,24 @@ INSERT INTO `atividade_turma` (`ID_atividade`, `ID_turma`) VALUES
 
 CREATE TABLE `aviso` (
   `ID` int(20) NOT NULL,
-  `Título` varchar(50) NOT NULL,
-  `Texto` varchar(100) NOT NULL,
-  `Data` varchar(20) NOT NULL,
+  `Titulo` varchar(150) CHARACTER SET utf8 NOT NULL,
+  `Texto` varchar(1000) CHARACTER SET utf8 NOT NULL,
+  `Data` varchar(20) CHARACTER SET utf8 NOT NULL,
   `ID_Turma` int(20) NOT NULL,
-  `Nome_Professor` varchar(50) NOT NULL
+  `Nome_Professor` varchar(50) CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `aviso`
+--
+
+INSERT INTO `aviso` (`ID`, `Titulo`, `Texto`, `Data`, `ID_Turma`, `Nome_Professor`) VALUES
+(3, 'teste', ' teste', '23/05/2017', 7, 'Professor'),
+(4, 'Teste', ' Teste1', '23/05/2017', 8, 'Professor'),
+(5, 'Teste', ' Teste1', '23/05/2017', 10, 'Professor'),
+(6, 'Uefs paralisada', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eleifend tortor nec dui posuere, eu ', '01/06/2017', 7, 'Professor'),
+(9, 'aa', ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eleifend tortor nec dui posuere, eu vulputate sapien blandit. Suspendisse imperdiet, elit nec mollis vestibulum, odio elit efficitur mi, sed fringilla magna metus sed turpis. Mauris a diam convallis erat consectetur porttitor varius id velit. Duis laoreet tincidunt lorem, ut auctor urna pellentesque in. Proin sed odio et dolor scelerisque elementum. ', '04/06/2017', 10, 'Professor'),
+(10, 'Lorem Ipsum', ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eleifend tortor nec dui posuere, eu vulputate sapien blandit. Suspendisse imperdiet, elit nec mollis vestibulum, odio elit efficitur mi, sed fringilla magna metus sed turpis. Mauris a diam convallis erat consectetur porttitor varius id velit. Duis laoreet tincidunt lorem, ut auctor urna pellentesque in. Proin sed odio et dolor scelerisque elementum.', '04/06/2017', 8, 'Professor');
 
 -- --------------------------------------------------------
 
@@ -114,7 +125,7 @@ CREATE TABLE `aviso` (
 CREATE TABLE `bloco_linhas` (
   `ID_atividade` int(10) NOT NULL,
   `Bloco` int(10) NOT NULL,
-  `texto` varchar(10000) NOT NULL
+  `texto` varchar(10000) CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -122,21 +133,15 @@ CREATE TABLE `bloco_linhas` (
 --
 
 INSERT INTO `bloco_linhas` (`ID_atividade`, `Bloco`, `texto`) VALUES
-(11, 1, 'leia a;\r\nleia b;\r\nleia c;'),
-(11, 2, 'soma = a + b+ c;'),
-(11, 3, 'escreva c;'),
-(12, 1, 'leia a;\r\nleia b;\r\nleia c;'),
-(12, 2, 'soma = a + b+ c;'),
-(12, 3, 'escreva c;'),
-(13, 1, 'leia a;\r\nleia b;\r\nleia c;'),
-(13, 2, 'soma = a + b+ c;'),
-(13, 3, 'escreva c;'),
-(14, 1, 'teste'),
-(14, 2, 'teste'),
-(14, 3, 'teste'),
 (15, 1, 'teste'),
 (15, 2, 'testando'),
-(15, 3, 'finalizando');
+(15, 3, 'finalizando'),
+(16, 1, 'int num1=5;\r\nint num2=4;\r\nint soma =0;'),
+(16, 2, 'soma = num1+num2;'),
+(16, 3, 'printf(soma);'),
+(17, 1, 'asdaf'),
+(17, 2, 'vefffw'),
+(17, 3, '12312xasd');
 
 -- --------------------------------------------------------
 
@@ -145,9 +150,9 @@ INSERT INTO `bloco_linhas` (`ID_atividade`, `Bloco`, `texto`) VALUES
 --
 
 CREATE TABLE `professor` (
-  `Nome` varchar(50) NOT NULL,
+  `Nome` varchar(100) CHARACTER SET utf8 NOT NULL,
   `Email` varchar(50) NOT NULL,
-  `Senha` varchar(50) NOT NULL,
+  `Senha` varchar(50) CHARACTER SET utf8 NOT NULL,
   `HashCode` int(50) NOT NULL,
   `Email Confirmado` tinyint(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -157,7 +162,7 @@ CREATE TABLE `professor` (
 --
 
 INSERT INTO `professor` (`Nome`, `Email`, `Senha`, `HashCode`, `Email Confirmado`) VALUES
-('Professor 2', 'professor2@gmail.com', '123456', 0, 0),
+('Professor 2', 'professor2@gmail.com', '123456', 0, 1),
 ('Professor', 'professor@gmail.com', '123456', 0, 1);
 
 -- --------------------------------------------------------
@@ -168,9 +173,9 @@ INSERT INTO `professor` (`Nome`, `Email`, `Senha`, `HashCode`, `Email Confirmado
 
 CREATE TABLE `turma` (
   `ID` int(20) NOT NULL,
-  `Disciplina` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `Professor` varchar(50) NOT NULL,
-  `Escola` varchar(50) NOT NULL
+  `Disciplina` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `Professor` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `Escola` varchar(50) CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -193,21 +198,24 @@ INSERT INTO `turma` (`ID`, `Disciplina`, `Professor`, `Escola`) VALUES
 
 CREATE TABLE `turma_alunos` (
   `ID_turma` int(20) NOT NULL,
-  `ID_aluno` varchar(50) NOT NULL
+  `ID_aluno` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `Atividade` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `Resolvido` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `Erradas` varchar(50) CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `turma_alunos`
 --
 
-INSERT INTO `turma_alunos` (`ID_turma`, `ID_aluno`) VALUES
-(0, 'teste@gmail.com'),
-(7, '3011rafael@gmail.com'),
-(7, 'gsm_fsa2008@hotmail.com'),
-(8, '3011rafael@gmail.com'),
-(8, 'gsm_fsa2008@hotmail.com'),
-(11, '3011rafael@gmail.com'),
-(11, 'gsm_fsa2008@hotmail.com');
+INSERT INTO `turma_alunos` (`ID_turma`, `ID_aluno`, `Atividade`, `Resolvido`, `Erradas`) VALUES
+(7, '3011rafael@gmail.com', '17', '', ''),
+(7, 'gsm_fsa2008@hotmail.com', '17', '', ''),
+(8, '3011rafael@gmail.com', '15', '', ''),
+(8, 'gsm_fsa2008@hotmail.com', '15', '', ''),
+(10, 'teste@gmail.com', '16,17', '', ''),
+(11, '3011rafael@gmail.com', '', '', ''),
+(11, 'gsm_fsa2008@hotmail.com', '', '', '');
 
 --
 -- Indexes for dumped tables
@@ -258,12 +266,12 @@ ALTER TABLE `turma_alunos`
 -- AUTO_INCREMENT for table `atividade`
 --
 ALTER TABLE `atividade`
-  MODIFY `ID` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `ID` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 --
 -- AUTO_INCREMENT for table `aviso`
 --
 ALTER TABLE `aviso`
-  MODIFY `ID` int(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT for table `turma`
 --
